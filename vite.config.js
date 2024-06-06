97ec86592df4f3810e77a12c3fb7058d3bdc8093
Ruben.Gabrielyan <Ruben.Gabrielyan@ameriabank.am>
2024-06-06 12:57:15 +0400

    Sdsdsdsd

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig({
   plugins: [react(), basicSsl()],
   build: {
-    outDir: './public'
+    outDir: './dist'
   },
   base: './'
 })

148060c29f2b2cd6989f7d4c450358efe13890a4
Ruben.Gabrielyan <Ruben.Gabrielyan@ameriabank.am>
2024-06-07 13:39:22 +0400

    sdsd

--- src/Pages/Friends.jsx
+++ src/Pages/Friends.jsx
@@ -1,12 +1,23 @@
 import WebApp from "@twa-dev/sdk";
+import {useEffect} from "react";
+import axios from "axios";
 
 
 export default function Friends () {
 
+    useEffect(() => {
+        axios.get(`https://bot.pinetech.org/api/register-user?id=${WebApp?.initDataUnsafe?.user?.id}`).then((res)=>{
+            console.log(res)
+        })
+    }, []);
+
     return (
         <div>
-            <h2>Friends</h2>
-            <a href={`https://telegram.me/share/url?url=t.me/tree_claimer_bot/app?startapp=${WebApp?.initDataUnsafe?.user?.id}`}>Invite</a>
+            <h2>Invite Friends</h2>
+            <button>
+                <a href={`https://telegram.me/share/url?url=t.me/tree_claimer_bot/app?startapp=${WebApp?.initDataUnsafe?.user?.id}`}>Invite</a>
+            </button>
+            <h3>My friends</h3>
         </div>
     )
 }

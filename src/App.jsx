950e8e7b6c1206a44fbbf6443b25557846d2e929
Ruben.Gabrielyan <Ruben.Gabrielyan@ameriabank.am>
2024-06-07 13:25:08 +0400

    sdsd

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
         }).then((res) => {
             setUser(res.data);
         }).catch((e)=>{
-            alert(JSON.stringify(e))
+            alert(JSON.stringify(e).substring(499))
         })
     }, []);
 

8e1b1e8fe047223bdc9175c9acba905d667908ea
Ruben.Gabrielyan <Ruben.Gabrielyan@ameriabank.am>
2024-06-07 09:57:18 +0400

    Adads

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import './App.css'
 import Tree from "./components/Tree.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import WebApp from "@twa-dev/sdk";
-import Menu from "./components/Menu.jsx";
+import axios from "axios";
 
 
 function App() {
     const [timeLeft, setTimeLeft] = useState(10); // Initialize timer with 60 seconds
+    const userId = WebApp?.WebAppUser?.id || 222;
+
+    useEffect(() => {
+        axios.post('https://bot.pinetech.org/register-user',{id:userId}).then((res)=>{
+            console.log(res)
+        })
+    }, []);
 
     return (
         <>
             <div>
                 <h1>{WebApp?.WebAppInitData?.user?.first_name || 'Ruben'}</h1>
+                <h2>{userId}</h2>
                 <h2>Your balance <span>4000</span></h2>
                 <Tree timerLeft={timeLeft} setTimeLeft={setTimeLeft}/>
                 <button className={'claim-button'} disabled={timeLeft > 0}>Claim</button>

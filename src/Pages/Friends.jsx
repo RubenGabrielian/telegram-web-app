024081909e3247eaa9db4e1d5cab075e80f0a6a3
Ruben.Gabrielyan <Ruben.Gabrielyan@ameriabank.am>
2024-06-07 11:35:13 +0400

    changes

--- src/Pages/Friends.jsx
+++ src/Pages/Friends.jsx
@@ -1,7 +1,15 @@
+import { initUtils } from '@tma.js/sdk';
+
+const utils = initUtils();
+
 export default function Friends () {
+    const handleInvite = () => {
+        utils.shareURL('t.me/tree_claimer_bot/app?ref=1212121', 'Look! Some cool app here!');
+    }
     return (
         <div>
             <h2>Friends</h2>
+            <button onClick={handleInvite}>Invite</button>
         </div>
     )
 }

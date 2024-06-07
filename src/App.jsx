a1b7dc0fef00b0f6f82fc01f124de28f132ee03b
Ruben.Gabrielyan <Ruben.Gabrielyan@ameriabank.am>
2024-06-07 10:18:12 +0400

    syernam

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     const [timeLeft, setTimeLeft] = useState(10); // Initialize timer with 60 seconds
     const userId = WebApp?.WebAppUser?.id || 222;
     const [user, setUser] = useState(null);
-    const telegramUser = WebApp?.initDataUnsafe?.user || {id: 222, first_name: 'Ruben', last_name: 'Gabrielyan'};
+    const telegramUser = WebApp?.initDataUnsafe?.user || {id: 222, first_name: 'Ruben', last_name: 'Gabrielyan',username: 'rubenTest'};
 
     useEffect(() => {
         axios.post('https://bot.pinetech.org/api/register-user', telegramUser).then((res) => {
@@ -26,7 +26,7 @@ function App() {
                 <div>
                     <h1>{WebApp?.WebAppInitData?.user?.first_name || 'Ruben'}</h1>
                     <h2>{userId}</h2>
-                    <h2>Your balance <span>${user?.count}</span></h2>
+                    <h2>Your balance <span>{user?.count}</span></h2>
                     <Tree timerLeft={timeLeft} setTimeLeft={setTimeLeft}/>
                     <button className={'claim-button'} disabled={timeLeft > 0}>Claim</button>
                 </div>
